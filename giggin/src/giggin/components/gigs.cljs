bdbc9376dab5537babee1f8a0fb4b79ce7348ca6
shinseitaro <shinsei.all@gmail.com>
2018-07-24 15:41:19 +0900

    first commit

---
+++ giggin/src/giggin/components/gigs.cljs
@@ -0,0 +1,17 @@
+(ns giggin.components.gigs
+  (:require [giggin.state :as state]))
+
+(defn gigs
+  []
+  [:main 
+     [:div.gigs (for  [{:keys [id artist img title price desc ]} (vals @state/gigs)]
+                     [:div.gig {:key id}
+                      [:img.gig__artwork {:src img :alt title}]
+                      [:div.gig__body
+                       [:div.gig__title
+                        [:div.btn.btn--primary.float--right.tooltip 
+                        {:data-tooltip "Add to order"
+                         :on-click #(swap! state/orders update id inc)}
+                         [:i.icon.icon--plus]] title]
+                       [:p.gig__price price]
+                       [:p.gig__desc desc]]]) ]])
